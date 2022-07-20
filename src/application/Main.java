bb3a1a5fabde04e713dfe607b944913b8b688b20
kanika.kapila1 <kanika.kapila1@IC-CS233-30.uc.ucalgary.ca>
2022-07-19 21:32:12 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-
+ 
 
 public class Main extends Application {
 	@Override
