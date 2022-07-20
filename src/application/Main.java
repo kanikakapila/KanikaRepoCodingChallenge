e44db142b7075580795509c1633adc5283eed67f
kanika.kapila1 <kanika.kapila1@IC-CS233-30.uc.ucalgary.ca>
2022-07-19 21:19:49 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,7 @@ import javafx.scene.layout.VBox;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) { 
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Zawad's Label")); 
